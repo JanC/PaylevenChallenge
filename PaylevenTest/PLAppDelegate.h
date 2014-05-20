1ca2e329adb86f10c9be12e9db790ec50e0f086a
Deepika Gopal <deepika.gopal@payleven.de>
2014-05-20 10:11:51 +0200

    Initial Commit

---
+++ PaylevenTest/PLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PLAppDelegate.h
+//  PaylevenTest
+//
+//  Created by dev on 20/05/14.
+//  Copyright (c) 2014 dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PaylevenTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PaylevenTest
+//
+//  Created by dev on 20/05/14.
+//  Copyright (c) 2014 dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PLAppDelegate class]));
+    }
+}
