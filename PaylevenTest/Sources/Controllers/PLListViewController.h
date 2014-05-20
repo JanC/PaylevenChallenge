a87c08518b14c7a3cb02cb9f87c0896c09a08104
Deepika Gopal <deepika.gopal@payleven.de>
2014-05-20 10:43:47 +0200

    init impl

---
+++ PaylevenTest/Sources/Controllers/PLListViewController.h
@@ -0,0 +1,10 @@
+//
+// Created by dev on 20/05/14.
+// Copyright (c) 2014 dev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface PLListViewController : UIViewController
+@end
---
+++ PaylevenTest/Sources/Controllers/PLListViewController.m
@@ -0,0 +1,12 @@
+//
+// Created by dev on 20/05/14.
+// Copyright (c) 2014 dev. All rights reserved.
+//
+
+#import "PLListViewController.h"
+
+
+@implementation PLListViewController {
+
+}
+@end
